1b7564fd57c5cde4f683f55867ae57f3586da92a
Raghav Shiv <Raghav@Raghavs-MacBook-Air.local>
2019-05-23 23:21:11 -0700

    Added if to eliminate input repetition

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,32 +3,37 @@
 
 
 
-
-var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+// Defining variables.
+var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var wins = 0;
 var losses = 0;
-var guessesLeft = 5;
+var guessesLeft = 9;
 var lettersGuessed = [];
 var computerGuess = [];
+// var alphabet = "abcdefghijklmnopqrstuvwxyz";
 
 
 
 
-
-function randomnum(){
-    computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length-1)];
-    // console.log(computerGuess);
+// Function to create random letter choices.
+function randomlettr() {
+    computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length )];
 
 }
-console.log(computerGuess);
+// Function to reset the game to original settings.
 function resetgame() {
 
-    guessesLeft = 5;
+    guessesLeft = 9;
     lettersGuessed = [];
-    randomnum();
-
+    randomlettr();
 }
 
+
+// function letGuess(){
+
+// }
+
+// Defined variables to hold text to push to the html.
 var winsText = document.getElementById("Wins");
 var lossesText = document.getElementById("Losses");
 var leftGues = document.getElementById("Guesses Left");
@@ -38,31 +43,36 @@ var guesses = document.getElementById("Guesses so far");
 document.onkeyup = function (event) {
 
     var userGuess = event.key;
-
-
-
-
+    // console.log (userGuess);
 
     if
-        (computerGuess === userGuess) {
-        wins++;
-        // console.log(computerGuess);
-        resetgame();
+        (computerChoices.includes(userGuess) && lettersGuessed.includes(userGuess) === false){
+            lettersGuessed.push(userGuess);
+            guessesLeft--;
 
+
+        // (alphabet.includes(userGuess)) {
+
+        if
+            (computerGuess === userGuess) {
+            wins++;
+            resetgame();
+        }
+
+        // else {
+            // guessesLeft--;
+            // lettersGuessed.push(" " + userGuess);
+        // }
+
+        else if
+            (guessesLeft === 0) {
+            losses++;
+            resetgame()
+        }
     }
-    else {
 
 
-        guessesLeft--;
-        lettersGuessed.push(" " + userGuess);
-    }
 
-    if 
-    (guessesLeft === 0){
-        losses++;
-        resetgame()
-
-    }
 
     winsText.textContent = wins;
     guesses.textContent = lettersGuessed;
